832c53d7708019f0effed44b72f5e644ff315397
Vivek Nath <59837356+mightyvivek@users.noreply.github.com>
2021-07-22 12:37:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyqrcode
+import png
+from pyqrcode import QRCode
+
+s = "https://github.com/mightyvivek"
+#img = Vivek.jpeg 
+
+url = pyqrcode.create(s, mode='binary')
+#url.svg("myqr.svg", scale = 8)
+url.png('myqr3.png', scale = 6)
+#url.show()
+
